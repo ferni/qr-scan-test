6c9bd9cfaeef92790b45514aeadbc1eaf45e4b66
Fernando Tiberti <fertiberti@gmail.com>
2020-09-24 19:54:37 -0300

    Add qr-reader.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import QrTest from "./QrTest"
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <QrTest />
     </div>
   );
 }
