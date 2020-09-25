f0c6f79ec7063a9a9d079df13b20e3d31eb87600
Fernando Tiberti <fertiberti@gmail.com>
2020-09-25 00:30:04 -0300

    Use Maker Neue theme.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React ,{useState} from 'react';
 import './App.css';
 import ScanQrButton from "./ScanQrButton"
+import { ThemeProvider, Text } from 'theme-ui'
+import theme from '@makerdao/dai-ui-theme-maker-neue'
 
 function App() {
   const [scanResult, setScanResult] = useState()
   return (
-    <div className="App">
-      {scanResult && <div>
-        You scanned: <strong>{scanResult}</strong>
-      </div>}
-      <ScanQrButton onScan={data => setScanResult(data)} onError={err => alert('Error: ' + err)}>
-        SCAN ADDRESS
-      </ScanQrButton>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        {scanResult && <Text>
+          You scanned: <strong>{scanResult}</strong>
+        </Text>}
+        <ScanQrButton
+          onScan={data => setScanResult(data)}
+          onError={err => alert('Error: ' + err)}
+          sx={{width: '145px', margin: '40% auto 0'}}
+        >
+          SCAN ADDRESS
+        </ScanQrButton>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/ScanQrButton.js
+++ src/ScanQrButton.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import QrReader from 'react-qr-reader'
 import { ReactComponent as CrossIcon} from "./crossIcon.svg"
-import { Box } from 'theme-ui'
+import { Box, Button } from 'theme-ui'
 
 const ScanQrButton = ({onScan, onError, children, ...props}) => {
   const [scanning, setScanning] = useState(false)
@@ -19,11 +19,11 @@ const ScanQrButton = ({onScan, onError, children, ...props}) => {
   }
 
   return (
-    <div>
+    <Box {...props}>
       {scanning &&
-      <Box sx={{position: 'fixed', top: '0', width: '100%', height: '100%', bg: 'black'}}>
+      <Box sx={{position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', bg: 'black'}}>
         <Box onClick={() => setScanning(false)}
-          sx={{position: 'absolute', top: '0', right: '0', zIndex: '200'}}>
+          sx={{position: 'absolute', top: 0, right: 0, zIndex: '200'}}>
           <CrossIcon width="40px" height="40px" style={{fill: "#fff"}} />
         </Box>
         <QrReader
@@ -32,10 +32,10 @@ const ScanQrButton = ({onScan, onError, children, ...props}) => {
           onScan={handleScan}
         />
       </Box>}
-      <button onClick={() => setScanning(true)} {...props}>
+      <Button onClick={() => setScanning(true)}>
         {children}
-      </button>
-    </div>
+      </Button>
+    </Box>
   )
 }
 
